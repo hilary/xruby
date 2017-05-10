9f3db4a0a1c0cd4679d1be4fcd1df12335d328f6
Geoff Hubbard <ghub@instihost.com>
2017-05-10 10:56:01 +0100

    Add tests for ComandLine
    100% coverage of CommandLine
    `parse` is the only public method.

--- test/generator/command_line_test.rb
+++ test/generator/command_line_test.rb
@@ -6,5 +6,33 @@ module Generator
       metadata: 'test/fixtures/metadata',
       track: 'test/fixtures/xruby'
     )
+
+    def test_parse_single
+      options = %w(alpha)
+      expected = ['alpha']
+      result = CommandLine.new(FixturePaths).parse(options)
+      assert_equal expected, result.map(&:exercise_name)
+    end
+
+    def test_parse_all
+      options = %w(--all)
+      expected = %w(alpha beta)
+      result = CommandLine.new(FixturePaths).parse(options)
+      assert_equal expected, result.map(&:exercise_name)
+    end
+
+    def test_parse_freeze
+      options = %w(--freeze alpha)
+      expected = [Generator::GenerateTests]
+      result = CommandLine.new(FixturePaths).parse(options)
+      assert_equal expected, result.map(&:class)
+    end
+
+    def test_parse_no_freeze
+      options = %w(alpha)
+      expected = [Generator::UpdateVersionAndGenerateTests]
+      result = CommandLine.new(FixturePaths).parse(options)
+      assert_equal expected, result.map(&:class)
+    end
   end
 end
